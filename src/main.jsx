3c864aa9b3a59088cc07c705749a98475b125cfc
Spink994 <ayodeji.bakare@sourcemygadgets.com>
2022-11-15 06:35:42 +0100

    Project Setup
    Set up the project with the basic requirements

---
+++ src/main.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import chatReducer from "./features/chatSlice";
+
+const store = configureStore({
+  reducer: {
+    chat: chatReducer,
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
